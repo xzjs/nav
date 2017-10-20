aace5cbac6641e42d8deaced924f001a9131a7ae
血之君殇 <2310976219@qq.com>
2017-10-20 16:08:42 +0800

    将小车上传摄像头的方式由zmq改为ros pub

--- src/cam_pub.py
+++ src/cam_pub.py
@@ -1,40 +1,49 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 '''cam ROS Node'''
-# license removed for brevity
-import rospy
+import pickle
+
 import cv2
 import numpy as np
-import pickle
-import zmq
+import roslib
+# license removed for brevity
+import rospy
+from sensor_msgs.msg import CompressedImage
 
 cap = cv2.VideoCapture(0)
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
 # socket.bind("tcp://*:5555")
-socket.connect("tcp://172.18.29.153:5555")
+socket.connect("tcp://192.168.31.5:5555")
 
 
 def talker():
     '''cam Publisher'''
-    # pub = rospy.Publisher('camera_data', String, queue_size=10)
-    rospy.init_node('my_cam', anonymous=True)
+    pub = rospy.Publisher('camera_data', CompressedImage, queue_size=10)
+
     rate = rospy.Rate(5)
     while not rospy.is_shutdown():
         ret, frame = cap.read()
         # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # cv2.imshow('frame', gray)
         res = cv2.resize(frame, (320, 240), interpolation=cv2.INTER_CUBIC)
-        cv2.imwrite("/tmp/camera.jpg", res)
-        f = open('/tmp/camera.jpg', 'rb')  # 读取摄像头图片
-        data = f.read() + '---' + 'cam'
-        socket.send(data)
+        # cv2.imwrite("/tmp/camera.jpg", res)
+        # f = open('/tmp/camera.jpg', 'rb')  # 读取摄像头图片
+        # data = f.read() + '---' + 'cam'
+        # socket.send(data)
+        msg=CompressedImage()
+        msg.header.stamp=rospy.Time.now()
+        msg.format="jpeg"
+        msg.data=np.array(cv2.imencode('.jpg',res)[1]).tostring()
+        pub.publish(msg)
         rospy.loginfo('upload success')
         rate.sleep()
 
 
 if __name__ == '__main__':
     try:
+        ic = image_feature()
+        rospy.init_node('my_camera', anonymous=True)
         rospy.loginfo('start canera')
         talker()
     except rospy.ROSInterruptException:
