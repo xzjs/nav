6d8fae3fd44d69835b1e5c4cd7d4ee2db561715b
血之君殇 <2310976219@qq.com>
2017-11-15 14:53:19 +0800

    更新端口记录文档
    去除调试信息

--- src/pointcloud_sub.py
+++ src/pointcloud_sub.py
@@ -15,10 +15,8 @@ def callback(data):
     # byte_data = pickle.dumps(data)
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
-    # socket.connect("tcp://172.18.29.153:5555")
-    socket.connect("tcp://192.168.31.5:5560")
-    # s = struct.pack('i', 4) + byte_data
-    # socket.send(s)
+    socket.connect("tcp://172.18.29.153:5555")
+    # socket.connect("tcp://192.168.31.5:5560")
     socket.send_pyobj(data)
     response = socket.recv()
     print "point", response
